7f7bde691652c891de1e33be4c42776cb03c6eda
GulSam00 <gulsamcono@gmail.com>
2025-01-10 14:35:55 +0900

    fix : 사용하지 않는 onRouteChange 메서드 주석화

--- app/src/App.js
+++ app/src/App.js
@@ -55,13 +55,16 @@ export default class App extends Component {
             pages.gamelog();
         });
 
-        router.onRouteChange = (currentRoute) => {
-            if (currentRoute === '#/') {
-                $header.style.display = 'none';
-            } else {
-                $header.style.display = 'block';
-            }
-        };
+        // 사용되지 않는 메서드
+        // router.onRouteChange = (currentRoute) => {
+        //     console.log('onRouteChange');
+        //     console.log('currentRoute', currentRoute);
+        //     if (currentRoute === '#/') {
+        //         $header.style.display = 'none';
+        //     } else {
+        //         $header.style.display = 'block';
+        //     }
+        // };
 
         router.start();
     }
