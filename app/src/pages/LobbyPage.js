c0051943ccd5f100a32dc2937c4f731120d40f1d
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2025-01-27 15:49:38 +0900

    feat: Select page

--- app/src/pages/LobbyPage.js
+++ app/src/pages/LobbyPage.js
@@ -29,13 +29,13 @@ export default class LobbyPage extends Component {
         const roomItems = rooms
             .map(
                 (room) => `
-            <li class="col m-auto rounded-5 d-flex flex-column p-3" 
-                style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
-                <div class="my-auto fs-5 text-white">${room.capacity}</div>
-                <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
-                <div class="m-auto fs-4 text-white">${room.roomName}</div>
-            </li>
-        `,
+                    <li class="col m-auto rounded-5 d-flex flex-column p-3" 
+                        style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                        <div class="my-auto fs-5 text-white">${room.capacity}</div>
+                        <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                        <div class="m-auto fs-4 text-white">${room.roomName}</div>
+                    </li>
+                `,
             )
             .join('');
 
