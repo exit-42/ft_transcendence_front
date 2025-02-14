1a290639c759b15eacaf92c36a5bf5f1fcbb370b
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2025-02-14 15:45:15 +0900

    fix : UI fix
     - 토너먼트 room UI 어색하게 보이는 현상 수정

--- app/src/components/UserCard.js
+++ app/src/components/UserCard.js
@@ -10,7 +10,7 @@ export default class UserCard extends Component {
 
         return `
             <div class="d-flex flex-column m-auto rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.6);">
-                <img class="m-auto rounded-circle" style="width: 10rem; height: 10rem; background-color: white" src="src/imgs/sample.jpeg">
+                <img class="m-auto rounded-circle cus-user-picture" src="src/imgs/sample.jpeg">
                 <div class="m-auto text-center fs-1 text-white fw-bold">${user.name}</div>
                 <div class="m-auto text-center fs-1 text-white fw-bold">${user.win}승 ${user.lose}패</div>
             </div>
--- app/src/pages/RoomFourPage.js
+++ app/src/pages/RoomFourPage.js
@@ -2,62 +2,56 @@ import Component from '../core/Component.js';
 import { UserCard } from '../components/index.js';
 
 export default class RoomFourPage extends Component {
-    template() {
-        return `
-            <div class="p-5" style="width: 100%; height: 88vh;">
-                    <div class="cus-tournament-container">
-                        <img class="cus-crown" src="src/imgs/crown.png"></img>
-                        <div class="cus-column-line"></div>
-                        <div class="cus-row-up-line"></div>
-                        <div class="cus-column-line-container">
-                            <div class="cus-column-line"></div>
-                            <div class="cus-column-line"></div>
-                        </div>
-                        <div class="cus-row-down-line-container">
-                            <div class="cus-row-down-line"></div>
-                            <div class="cus-row-down-line"></div>
-                        </div>
-                        <div class="cus-column-line-container">
-                            <div class="cus-column-line"></div>
-                            <div class="cus-column-line"></div>
-                            <div class="cus-column-line"></div>
-                            <div class="cus-column-line"></div>
-                        </div>
-                    </div>
-                    <div class="cus-room-container d-flex justify-content-evenly" style="width: 100%;">
-                        <div data-component="usercard" class="cus-user-card">
-                        </div>
-                        <div data-component="usercard" class="cus-user-card">
-                        </div>
-                        <div data-component="usercard" class="cus-user-card">
-                        </div>
-                        <div data-component="usercard" class="cus-user-card">
-                        </div>
-                    </div>
-                        <div class="d-flex m-5">
-                            <button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="exit-button">Exit</button>
-                            <button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="start-button">Start</button>
-                        </div>
+	template() {
+		return `
+			<div class="p-5" style="width: 100%; height: 88vh;">
+				<div class="cus-tournament-container">
+					<img class="cus-crown" src="src/imgs/crown.png"></img>
+					<div class="cus-column-line"></div>
+					<div class="cus-row-up-line"></div>
+					<div class="cus-column-line-container">
+						<div class="cus-column-line"></div>
+						<div class="cus-column-line"></div>
+					</div>
+					<div class="cus-row-down-line-container">
+						<div class="cus-row-down-line"></div>
+						<div class="cus-row-down-line"></div>
+					</div>
+					<div class="cus-column-line-container">
+						<div class="cus-column-line"></div>
+						<div class="cus-column-line"></div>
+						<div class="cus-column-line"></div>
+						<div class="cus-column-line"></div>
+					</div>
+				</div>
+				<div class="cus-room-container d-flex justify-content-around" style="width: 100%;">
+					<div data-component="usercard" class="cus-user-card"></div>
+					<div data-component="usercard" class="cus-user-card"></div>
+					<div data-component="usercard" class="cus-user-card"></div>
+					<div data-component="usercard" class="cus-user-card"></div>
+				</div>
+				<div class="d-flex m-5">
+					<button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="exit-button">Exit</button>
+					<button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="start-button">Start</button>
+				</div>
+			</div>
+		`;
+	}
 
+	mounted() {
+		const $usercards = this.$target.querySelectorAll('[data-component="usercard"]');
+		$usercards.forEach(($usercard) => {
+			new UserCard($usercard);
+		});
+	}
 
-            </div>
-        `;
-    }
+	setEvent() {
+		this.addEvent('click', '#exit-button', () => {
+			window.location.hash = '/lobby';
+		});
 
-    mounted() {
-        const $usercards = this.$target.querySelectorAll('[data-component="usercard"]');
-        $usercards.forEach(($usercard) => {
-            new UserCard($usercard);
-        });
-    }
-
-    setEvent() {
-        this.addEvent('click', '#exit-button', () => {
-            window.location.hash = '/lobby';
-        });
-
-        this.addEvent('click', '#start-button', () => {
-            window.location.hash = '/game';
-        });
-    }
+		this.addEvent('click', '#start-button', () => {
+			window.location.hash = '/game';
+		});
+	}
 }
