128742742022a080a2d1fccf5376a7bf202444fe
GulSam00 <gulsamcono@gmail.com>
2025-02-22 21:09:43 +0900

    chore : unused 함수 주석

--- app/src/api/useFetch.js
+++ app/src/api/useFetch.js
@@ -16,28 +16,28 @@ export async function useFetch(url, options = {}) {
     }
 }
 
-function handleError(status, errorData) {
-    console.log('errorData', errorData);
-    switch (status) {
-        case 400:
-            alert('잘못된 요청입니다');
-            break;
-        case 401:
-            alert('인증이 필요합니다');
-            break;
-        case 403:
-            alert('접근이 거부되었습니다');
-            break;
-        case 404:
-            alert('페이지를 찾을 수 없습니다');
-            break;
-        case 409:
-            alert('이미 존재하는 데이터입니다');
-            break;
-        case 500:
-            alert('서버 에러');
-            break;
-        default:
-            alert('알 수 없는 에러');
-    }
-}
+// function handleError(status, errorData) {
+//     console.log('errorData', errorData);
+//     switch (status) {
+//         case 400:
+//             alert('잘못된 요청입니다');
+//             break;
+//         case 401:
+//             alert('인증이 필요합니다');
+//             break;
+//         case 403:
+//             alert('접근이 거부되었습니다');
+//             break;
+//         case 404:
+//             alert('페이지를 찾을 수 없습니다');
+//             break;
+//         case 409:
+//             alert('이미 존재하는 데이터입니다');
+//             break;
+//         case 500:
+//             alert('서버 에러');
+//             break;
+//         default:
+//             alert('알 수 없는 에러');
+//     }
+// }
