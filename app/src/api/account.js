35b3408797949e70a4669fd1a7209f2400c3e97a
GulSam00 <gulsamcono@gmail.com>
2025-02-22 20:46:53 +0900

    Fix : 확장자 에러 이슈 해결

--- app/src/api/account.js
+++ app/src/api/account.js
@@ -1,4 +1,4 @@
-import { useFetch } from './useFetch';
+import { useFetch } from './useFetch.js';
 
 const baseUrl = 'account/';
 export async function postLogout() {
@@ -8,7 +8,7 @@ export async function postLogout() {
 }
 
 export async function postLogin() {
-    const response = await useFetch(baseUrl + '/login/', { method: 'POST' });
+    const response = await useFetch(baseUrl + 'login/', { method: 'POST' });
 
     return response;
 }
