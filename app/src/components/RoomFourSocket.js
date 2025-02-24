3558d272d6016ec784eb601b74e41c9d18c09c41
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2025-02-24 14:16:22 +0900

    feat : room socket 적용
     - room socket 적용
     - 백엔드와 협업 후 추가작업 필요

---
+++ app/src/components/RoomFourSocket.js
@@ -0,0 +1,168 @@
+import Component from '../core/Component.js';
+import playGame from '../game/GameModule.js';
+
+export default class RoomFourSocket extends Component {
+    template() {
+        const player1 = 'heolee';
+        const player2 = 'haejeong';
+
+        const user1 = {
+            name: 'heolee',
+            win: 1,
+            lose: 0,
+        };
+
+        const user2 = {
+            name: 'heolee',
+            win: 1,
+            lose: 0,
+        };
+
+        const user3 = {
+            name: 'heolee',
+            win: 1,
+            lose: 0,
+        };
+
+        const user4 = {
+            name: 'heolee',
+            win: 1,
+            lose: 0,
+        };
+
+        return `
+            <div class="p-5" style="width: 100%; height: 88vh;">
+                <div class="cus-tournament-container">
+                    <img class="cus-crown" src="src/imgs/crown.png"></img>
+                    <div class="cus-column-line"></div>
+                    <div class="cus-row-up-line"></div>
+                    <div class="cus-column-line-container">
+                        <div class="cus-column-line"></div>
+                        <div class="cus-column-line"></div>
+                    </div>
+                    <div class="cus-row-down-line-container">
+                        <div class="cus-row-down-line"></div>
+                        <div class="cus-row-down-line"></div>
+                    </div>
+                    <div class="cus-column-line-container">
+                        <div class="cus-column-line"></div>
+                        <div class="cus-column-line"></div>
+                        <div class="cus-column-line"></div>
+                        <div class="cus-column-line"></div>
+                    </div>
+                </div>
+                <div class="cus-room-container d-flex justify-content-around" style="width: 100%;">
+                    <div class="cus-user-card">
+                        <div class="d-flex flex-column m-auto rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.6);">
+                            <img class="m-auto rounded-circle cus-user-card-picture" src="src/imgs/sample.jpeg">
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.name}</div>
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.win}승 ${user1.lose}패</div>
+                        </div>
+                    </div>
+                    <div class="cus-user-card">
+                        <div class="d-flex flex-column m-auto rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.6);">
+                            <img class="m-auto rounded-circle cus-user-card-picture" src="src/imgs/sample.jpeg">
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.name}</div>
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.win}승 ${user1.lose}패</div>
+                        </div>
+                    </div>
+                    <div class="cus-user-card">
+                        <div class="d-flex flex-column m-auto rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.6);">
+                            <img class="m-auto rounded-circle cus-user-card-picture" src="src/imgs/sample.jpeg">
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.name}</div>
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.win}승 ${user1.lose}패</div>
+                        </div>
+                    </div>
+                    <div class="cus-user-card">
+                        <div class="d-flex flex-column m-auto rounded-5" style="width: 100%; height: 100%; background-color: rgba(14, 180, 252, 0.6);">
+                            <img class="m-auto rounded-circle cus-user-card-picture" src="src/imgs/sample.jpeg">
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.name}</div>
+                            <div class="m-auto text-center fs-1 text-white fw-bold">${user1.win}승 ${user1.lose}패</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex m-5">
+                    <button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="exit-button">Exit</button>
+                    <button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="start-button">Start</button>
+                </div>
+            </div>
+            <div class="px-5 pb-5" style="width: 100%; height: 88vh;">
+                <div class="fs-2 fw-normal d-flex align-items-center justify-content-center" style="width: 100%; height: 10%;">${player1}님과 ${player2}님이 경기중 입니다</div>
+                <div data-component="game" class="fs-1 m-auto cus-threejs">
+                    <canvas id="canvas" style="width: 100%; height: 100%;"></canvas>
+                </div>
+            </div>
+        `;
+    }
+
+    async mounted() {
+        this.$state = await playGame();
+        console.log(this.$state);
+        // this.$state.game.ball.position.set(1, 3, -1);
+        // this.$state.game.you.position.set(1, 1.9, 3.2);
+        // this.$state.game.enemy.position.set(-1, 1.9, -3.2);
+
+        console.log('소켓 열기');
+        // this.socket = new WebSocket('ws://localhost:8000');
+    }
+
+    dispose() {
+        console.log('소켓 닫기');
+        // if (this.socket) {
+        //     this.socket.close();
+        // }
+    }
+
+    setEvent() {
+        // // 메시지 수신
+        // this.socket.onmessage = (event) => {
+        //     console.log(event.data);
+        // };
+
+        // // 소켓 에러 발생
+        // this.socket.onerror = (error) => {
+        //     console.error(error);
+        // };
+
+        window.addEventListener('keydown', (event) => {
+            if (!this.$state.game.you) return;
+
+            const step = 0.15;
+            if (event.key === 'ArrowLeft') {
+                if (this.$state.game.you.position.x > -1.2) {
+                    this.$state.game.you.position.x -= step;
+
+                    // // 뭐라뭐라 보냄
+                    // this.socket.send(
+                    //     JSON.stringify({
+                    //         // type: 'move',
+                    //         // player: 'you',
+                    //         // position: this.$state.game.you.position
+                    //     }),
+                    // );
+                }
+            } else if (event.key === 'ArrowRight') {
+                if (this.$state.game.you.position.x < 1.2) {
+                    this.$state.game.you.position.x += step;
+
+                    // // 뭐라뭐라 보냄
+                    // this.socket.send(
+                    //     JSON.stringify({
+                    //         // type: 'move',
+                    //         // player: 'you',
+                    //         // position: this.$state.game.you.position
+                    //     }),
+                    // );
+                }
+            }
+        });
+
+        this.addEvent('click', '#exit-button', () => {
+            window.location.hash = '/lobby';
+        });
+
+        this.addEvent('click', '#start-button', () => {
+            window.location.hash = '/game';
+        });
+    }
+}
--- app/src/pages/RoomFourPage.js
+++ app/src/pages/RoomFourPage.js
@@ -1,57 +1,15 @@
 import Component from '../core/Component.js';
-import { UserCard } from '../components/index.js';
+import { RoomFourSocket } from '../components/index.js';
 
 export default class RoomFourPage extends Component {
     template() {
         return `
-			<div class="p-5" style="width: 100%; height: 88vh;">
-				<div class="cus-tournament-container">
-					<img class="cus-crown" src="src/imgs/crown.png"></img>
-					<div class="cus-column-line"></div>
-					<div class="cus-row-up-line"></div>
-					<div class="cus-column-line-container">
-						<div class="cus-column-line"></div>
-						<div class="cus-column-line"></div>
-					</div>
-					<div class="cus-row-down-line-container">
-						<div class="cus-row-down-line"></div>
-						<div class="cus-row-down-line"></div>
-					</div>
-					<div class="cus-column-line-container">
-						<div class="cus-column-line"></div>
-						<div class="cus-column-line"></div>
-						<div class="cus-column-line"></div>
-						<div class="cus-column-line"></div>
-					</div>
-				</div>
-				<div class="cus-room-container d-flex justify-content-around" style="width: 100%;">
-					<div data-component="usercard" class="cus-user-card"></div>
-					<div data-component="usercard" class="cus-user-card"></div>
-					<div data-component="usercard" class="cus-user-card"></div>
-					<div data-component="usercard" class="cus-user-card"></div>
-				</div>
-				<div class="d-flex m-5">
-					<button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="exit-button">Exit</button>
-					<button class="m-auto border-0 fs-1 text-white fw-bold rounded-pill cus-button" style="width: 13rem; height: 4rem;" id="start-button">Start</button>
-				</div>
-			</div>
+			<div data-component="socket"></div>
 		`;
     }
 
     mounted() {
-        const $usercards = this.$target.querySelectorAll('[data-component="usercard"]');
-        $usercards.forEach(($usercard) => {
-            new UserCard($usercard);
-        });
-    }
-
-    setEvent() {
-        this.addEvent('click', '#exit-button', () => {
-            window.location.hash = '/lobby';
-        });
-
-        this.addEvent('click', '#start-button', () => {
-            window.location.hash = '/game';
-        });
+        const $socket = this.$target.querySelector('[data-component="socket"]');
+        new RoomFourSocket($socket);
     }
 }
