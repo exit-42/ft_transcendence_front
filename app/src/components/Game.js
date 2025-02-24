ad708676a7ead684e259c38751415aa56a3531bb
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2025-02-24 14:13:59 +0900

    feat : 소켓 로직 적용

--- app/src/components/Game.js
+++ app/src/components/Game.js
@@ -13,14 +13,29 @@ export default class Game extends Component {
         // this.$state.ball.position.set(1, 3, -1);
         // this.$state.you.position.set(1, 1.9, 3.2);
         // this.$state.enemy.position.set(-1, 1.9, -3.2);
-        console.log('소켓열기');
+
+        console.log('소켓 열기');
+        // this.socket = new WebSocket('ws://localhost:8000');
     }
 
     dispose() {
-        console.log('소켓닫기');
+        console.log('소켓 닫기');
+        // if (this.socket) {
+        //     this.socket.close();
+        // }
     }
 
     setEvent() {
+        // // 메시지 수신
+        // this.socket.onmessage = (event) => {
+        //     console.log(event.data);
+        // };
+
+        // // 소켓 에러 발생
+        // this.socket.onerror = (error) => {
+        //     console.error(error);
+        // };
+
         window.addEventListener('keydown', (event) => {
             if (!this.$state.you) return;
 
@@ -28,10 +43,28 @@ export default class Game extends Component {
             if (event.key === 'ArrowLeft') {
                 if (this.$state.you.position.x > -1.2) {
                     this.$state.you.position.x -= step;
+
+                    // // 뭐라뭐라 보냄
+                    // this.socket.send(
+                    //     JSON.stringify({
+                    //         // type: 'move',
+                    //         // player: 'you',
+                    //         // position: this.$state.you.position
+                    //     }),
+                    // );
                 }
             } else if (event.key === 'ArrowRight') {
                 if (this.$state.you.position.x < 1.2) {
                     this.$state.you.position.x += step;
+
+                    // // 뭐라뭐라 보냄
+                    // this.socket.send(
+                    //     JSON.stringify({
+                    //         // type: 'move',
+                    //         // player: 'you',
+                    //         // position: this.$state.you.position
+                    //     }),
+                    // );
                 }
             }
         });
