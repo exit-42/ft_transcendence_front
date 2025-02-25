2f0831110b5963e990629d3938bbda1c74f923f2
이헌석 <109660331+HeonSeok98@users.noreply.github.com>
2025-02-25 11:02:01 +0900

    feat : lobby 페이지 컴포넌트 분리
     - looby 페이지 컴포넌트 분리
     - 추후 추가작업 필요

--- app/src/components/LobbyBox.js
+++ app/src/components/LobbyBox.js
@@ -2,8 +2,73 @@ import Component from '../core/Component.js';
 
 export default class LobbyBox extends Component {
     template() {
+        const rooms = [
+            {
+                capacity: '2/4',
+                roomName: "heolee's room",
+            },
+            {
+                capacity: '2/2',
+                roomName: "haejeong's room",
+            },
+            {
+                capacity: '1/2',
+                roomName: "sangyhan's room",
+            },
+            {
+                capacity: '1/4',
+                roomName: "sham's room",
+            },
+            {
+                capacity: '1/4',
+                roomName: "klha's room",
+            },
+            {
+                capacity: '3/4',
+                roomName: "dna's room",
+            },
+        ];
+
+        const roomItems = rooms
+            .map(
+                (room) => `
+                    <li class="col m-auto rounded-5 d-flex flex-column p-3" 
+                        style="width: 16rem; height: 12rem; background-color: rgba(14, 180, 252, 0.5);">
+                        <div class="my-auto fs-5 text-white">${room.capacity}</div>
+                        <div class="m-auto rounded-circle" style="width: 5rem; height: 5rem; background-color: white"></div>
+                        <div class="m-auto fs-4 text-white">${room.roomName}</div>
+                    </li>
+                `,
+            )
+            .join('');
+
         return `
-            <div></div>
+            <div class="cus-parent">
+                <div class="d-flex justify-content-evenly cus-mode-button-container">
+                    <button class="border-0 fs-4 text-white fw-bold rounded-pill cus-mode-button cus-button" id="room2-button">1 VS 1</button>
+                    <button class="border-0 fs-4 text-white fw-bold rounded-pill cus-mode-button cus-button" id="room4-button">Tournament</button>
+                </div>
+                <div class="inner-container">
+                    <ul class="cus-lobby-container">
+                        ${roomItems}
+                    </ul>
+                </div>
+            </div>
         `;
+    }
+
+    setup() {
+        const roomList = [];
+        this.$state = roomList;
+    }
+
+    setEvent() {
+        this.addEvent('click', '#room2-button', () => {
+            // console.log(2);
+        });
+
+        this.addEvent('click', '#room4-button', () => {
+            // console.log(4);
+        });
     }
 }
